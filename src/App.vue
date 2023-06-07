5d26029a9bfee5ddf4595791d9049dbbcfad2234
Chuck Smith <csmit223@nd.edu>
2023-06-07 09:10:00 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheFooter from "@/components/Footer.vue";
+</script>
+
+<template>
+  <h1>Hello world</h1>
+  <TheFooter />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
+  </footer>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "TheFooter"
+})
+</script>
+
+<style lang="scss" scoped>
+footer {
+  display: grid;
+  grid-area: footer;
+  align-items: center;
+  justify-content: center;
+
+
+  .attribution {
+    font-size: 11px;
+    color: var(--text-card-body);
+
+    a {
+      color: var(--color-primary);
+    }
+  }
+}
+
+</style>
