7d220682c66095cf4da0bf647bb492733391b6d9
Chuck Smith <csmit223@nd.edu>
2023-10-23 11:23:23 -0400

    Add footer link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <div class="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-      Coded by <a href="#">Your Name Here</a>.
+      Coded by <a href="https://www.linkedin.com/in/dev-chuck-smith/" target="_blank" aria-label="Dev profile (new window)">Chuck Smith</a>.
     </div>
   </footer>
 </template>
